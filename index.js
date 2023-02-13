0b7a409189a8daab84af81dccb548d4572fa16ed
nguyenvantien <nguyenvantien.study@gmail.com>
2023-02-13 17:20:36 +0700

    fix CORS

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const cors = require("cors")
 const port = process.env.PORT || 3000
 
 // Page Home
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+});
+
 app.get("/", (req, res) => {
     res.send('SERVER ON')
 })
